6b4bb60a26969efc157e9f3f625ae74d92d1b13d
carlos vergara pereira <charlie09042013@gmail.com>
2020-06-20 00:34:27 -0400

    Carlos
    parte uno usuario

---
+++ database/migrations/2020_06_20_031150_create_usuarios_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateUsuariosTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('usuarios', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('nombre');
+            $table->string('apellido');
+            $table->string('email');
+            $table->string('rol');
+            $table->string('pass');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('usuarios');
+    }
+}
