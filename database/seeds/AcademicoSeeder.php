18e3d9e5f6e093c86bdbd44d25ee33a305c14a96
carlos vergara pereira <charlie09042013@gmail.com>
2020-06-26 03:32:54 -0400

    Seeder Academico 1

--- database/seeds/AcademicoSeeder.php
+++ database/seeds/AcademicoSeeder.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Database\Seeder;
-
+use App\Academico;
 class AcademicoSeeder extends Seeder
 {
     /**
@@ -11,6 +11,18 @@ class AcademicoSeeder extends Seeder
      */
     public function run()
     {
-        //
+        $academico=new Academico();
+        $academico ->rut='11.111.111-1';
+        $academico->titulo='Doctor en Filosofia';
+        $academico->gradoAcademico='Doctor';
+        $academico->categoria='Full';
+        $academico->HorasContrato='45';
+        $academico->nombre='Cristhian Alejandro ';
+        $academico->apellido='Almonacid Diaz';
+        $academico->email='calmonacid@ucm.cl';
+        $academico->estado='1';
+        $academico->departamento_id='17';
+        $academico->save();
+
     }
 }
